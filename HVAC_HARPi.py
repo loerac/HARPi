eb4afd62f195e59253e258ac3f67375750caa363
Brennen Olson <olsonb0209@my.uwstout.edu>
2018-05-02 22:38:50 +0000

    Added MQTT .status_msg()

--- HVAC_HARPi.py
+++ HVAC_HARPi.py
@@ -1,14 +1,20 @@
+#!/usr/bin/python
+
 import BME280
 import RPi.GPIO as GPIO
 import time
-##from child_mqtt import child_mqtt
+from child_mqtt import child_mqtt
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
+NAME = "HVAC_HARPi"
+TOPIC = "Brennen/Temp"
+BROKER_IP = "HARPIO.DUCKDNS.ORG"
+TOKEN = "TEMP" 
+DESC =  "TEMP inside HVAC"
 
-##child = child_mqtt(NAME, TOPIC, BROKER_IP, TOKEN, DESC)
+HVAC_HARPi_Child = child_mqtt(NAME, TOPIC, BROKER_IP, TOKEN, DESC)
 
 coil_1_pin = 15  # orange
 coil_2_pin = 13  # yellow
@@ -36,31 +42,33 @@ def setStep(w1, w2, w3, w4):
     GPIO.output(coil_3_pin, w3)
     GPIO.output(coil_4_pin, w4)
  
-def forward(delay, steps):
+def open():
+    delay = 1/1000
+    steps = 128
     for i in range(steps): 
         for j in range(StepCount): 
             setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
             time.sleep(delay)
             
-def reverse(delay, steps):
+def close():
+    delay = 1/1000
+    steps = 128
     for i in range(steps):
         for j in range(StepCount):
             setStep(Seq[j][3], Seq[j][2], Seq[j][1], Seq[j][0])
             time.sleep(delay)
 
-while(StepCount==8):
-    stepPos = steps + stepPos
-    temperature,pressure,humidity = BME280.readBME280All()
-    delay = 1/1000
-    if(temperature < 25):
-        steps = 128 #128 steps = 90 degrees
-        forward(delay,steps)
-    if(temperature > 25):
-        steps = 128
-        reverse(delay,steps)
-        
-    print(temperature)
+def sendTemp(temperature):
+    HVAC_HARPi_Child.status_msg(temperature)
+
+temperature,pressure,humidity = BME280.readBME280All()
+open()
+close()
+sendTemp("{:.2f}".format(temperature))
+
+
     
 GPIO.cleanup()
+
    
     
